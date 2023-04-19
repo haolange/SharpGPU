af03c7c7a18f543e524339d8c41a535f183580e0
haolange <1824339952@qq.com>
2023-04-19 23:28:49 +0800

    Update

--- Abstract/RHICommandEncoder.cs
+++ Abstract/RHICommandEncoder.cs
@@ -130,7 +130,7 @@ namespace Infinity.Graphics
     {
         public string Name;
         public RHIShadingRateDescriptor? ShadingRateDescriptor;
-        //public Memory<RHISubpassDescriptor>? SubpassDescriptors;
+        internal Memory<RHISubpassDescriptor>? SubpassDescriptors;
         public Memory<RHIColorAttachmentDescriptor> ColorAttachmentDescriptors;
         public RHIDepthStencilAttachmentDescriptor? DepthStencilAttachmentDescriptor;
     }
